a642491600800c2faebfb5aec6a4ffa6c26054b2
Jennifer <jennifersequina@gmail.com>
2021-08-15 15:33:39 +0400

    updated getting link

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -17,19 +19,17 @@ try:
     result = WebDriverWait(driver, 10).until(
         ec.presence_of_element_located((By.ID, "search"))
     )
-    # print(element.text)
-#
-#     books = result.find_elements_by_class_name("sg-col-inner")
-#     for book in books:
-#         title = books.find_element_by_class_name("a-link-normal a-text-normal")
-#         # link = books.find_element_by_class_name("href")
-#         print(title)
-# #data component type="s-search-result"
-# # a class="a-link-normal a-text-normal"
-# # href=
+
+    books = result.find_elements_by_class_name("sg-col-inner")
+    for book in books:
+        # print(book.text)
+        links = book.find_elements_by_xpath("//a[@class='a-link-normal a-text-normal']")
+        # print(links)
+        for link in links:
+            print(link.get_attribute("href"))
 
 finally:
-    driver.quit()
+    None
+    # driver.quit()
 
 time.sleep(10)
-
